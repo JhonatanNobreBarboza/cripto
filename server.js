63eef1ccc02f1c0951add2a1cbdf5d1338e0008d
Jhonatan Nobre <jh_nbs@outlook.com>
2019-03-04 15:56:51 -0400

    implentação nodemon e função critpografar

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.listen(3000, function(){
 })
 
 app.get('/', (req, res)=>{
-    res.render('index.ejs')
+    res.render('index')
 })
 
 app.set('view engine', 'ejs')
--- views/js/functions.js
+++ views/js/functions.js
@@ -1,72 +1,28 @@
-$( document ).ready(function() {
-    $(".inline-editable").hover(
-         function() {$(this).addClass("editHover"); }, 
-         function() { $(this).removeClass("editHover"); }
-     );
-   
-   var oldText,
-       newText;
-  
-   $(".inline-editable").bind("click", replaceHTML)
-   
-   function replaceHTML() {
-       // console.log($(".editing"))
-       if(! $(".editing").length){
-         oldText = $(this).html()
-         $(this).addClass("editing");
-         var form = createForm(oldText);
-         $(this).html("")
-                  .html(form);
-         $(this).unbind('click', replaceHTML);
-        }
+module.exports.criptografar = function (aplication, req, res){
+const frase1 = "";
+const alfa = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const frase = prompt("Introduza a frase:").toLowerCase();
+const off = prompt("Introduza o deslocamento:");
+const x = parseInt(off);
+const keep = [];
+
+for (var i = 0; i < frase.length; i++)
+{
+ if(frase[i] != ' ')
+ {
+   for (var j = 0; j < alfa.length; j++)
+   {
+     if (frase[i] == alfa[j])
+     {
+       keep[i] = alfa[(j + x) % alfa.length];
+       break;
      }
-   
-   function createForm(text) {
-     var form = "<form class=\"inline-editor\">";
-     form += "<input type=\"text\" class=\"editBox\" value=\"";
-     form += oldText;
-     form += "\" /> </form>";
-     form += "<a href=\"#\" class=\"btnSave white-text btn \">Save</a>";
-     form += "<a href=\"#\" class=\"btnDiscard\">Cancel</a>";
-     return form
    }
-   
-   $(document.body).on("click", ".btnDiscard", function() {
-         $(this).parent().html(oldText);
-         $(".inline-editable").removeClass("editing");
-         $(".inline-editable").bind('click', replaceHTML);
-         $(".inline-editable").removeClass("editHover");
-   });
-   
-   $(document.body).on("click", ".btnSave", function() {
-        newText = $(this).parent() 
-                          .find(".editBox")
-                          .val();
-        saveChanges(this, newText)
-   });
-   
-   // use enter button
-   $(document.body).on("submit", ".inline-editor", function(e) {
-      e.preventDefault();
-      var newText = $(div).find(".editBox").val()
-      saveChanges(this, newText)
-   })
- 
-   function saveChanges(div, newText) {
-     
-     // get data for DB
-     var id =  $(div).parent().attr("id");
-     var dataToUpdate = { id : id, text : newText };
-     console.log(dataToUpdate)
-    
-     $(".inline-editable").removeClass("editing");
-     $(div).parent().html(newText);
-     $(".inline-editable").bind('click', replaceHTML);
-     $(".inline-editable").removeClass("editHover");
-     
-   }
-   
-   
-   
-   
- });
+ }
+ else
+ {
+   keep[i] = ' ';
+ }
+}
+alert(keep.join(""));
+}
